7e277a63326ecad6b9de0f70271b34d11cb12e0e
Etienne[에티엔] <etienne@plutusds.com>
2023-07-14 18:28:45 +0900

    setup react query provider

--- App.tsx
+++ App.tsx
@@ -9,23 +9,28 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components/native';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { store } from './src/store';
 import Root from './src/components/Root';
 import { darkTheme } from './src/themes/dark';
 import { lightTheme } from './src/themes/light';
 
+const queryClient = new QueryClient();
+
 function App(): JSX.Element {
   const isDarkMode = store.getState().app.theme === 'dark';
 
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-        <NavigationContainer>
-          <Root />
-        </NavigationContainer>
-      </ThemeProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+          <NavigationContainer>
+            <Root />
+          </NavigationContainer>
+        </ThemeProvider>
+      </Provider>
+    </QueryClientProvider>
   );
 }
 
